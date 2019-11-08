22f7aa2c3032a9f4d02f94db71e9a581db2ff6c6
Nick <nicholas_calabro@student.uml.edu>
2019-11-08 16:55:27 -0500

    initial commit

---
+++ src/cutils.h
@@ -0,0 +1,41 @@
+#ifndef CUTILS_H
+#define CUTILS_H
+
+typedef enum Status{
+	FAILURE, SUCCESS
+} Status;
+
+typedef enum Boolean{
+	FALSE, TRUE
+} Boolean;
+
+
+int cusearch_linear_int(int *array, unsigned int arrayCount, int number);
+int cusearch_linear_uint(unsigned int *array, unsigned int arrayCount, unsigned int number);
+int cusearch_linear_float(float *arrray, unsigned int arrayCount, float num);
+
+int cusearch_binary_int(int *array, unsigned int arrayCount, int number);
+int cusearch_binary_uint(unsigned int *array, unsigned int arrayCount, unsigned int number);
+int cusearch_binary_float(float *array, unsigned int arrayCount, float number);
+
+void cusort_bubble_int(int *array, unsigned int arrayCount);
+void cusort_bubble_uint(unsigned int *array, unsigned int arrayCount);
+void cusort_bubble_float(float *array, unsigned int arrayCount);
+
+void cusort_selection_int(int *array, unsigned int arrayCount);
+void cusort_selection_uint(unsigned int *array, unsigned int arrayCount);
+void cusort_selection_float(float *array, unsigned int arrayCount);
+
+void cusort_merge_int(int *array, unsigned int arrayCount);
+void cusort_merge_uint(unsigned int *array, unsigned int arrayCount);
+void cusort_merge_float(float *array, unsigned int arrayCount);
+
+
+void cuswap_int(int *a, int *b);
+void cuswap_uint(unsigned int *a, unsigned int *b);
+void cuswap_float(float *a, float *b);
+
+Boolean cucmp_float(float a, float b);
+Boolean cucmp_double(double a, double b);
+
+#endif
